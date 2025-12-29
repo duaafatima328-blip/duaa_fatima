8729c49f3319cc39b8977fc8d05c927f841d5661
duaafatima328-blip <duaafatima328@gmail.com>
2025-12-29 15:59:06 +0500

    Add files via upload

---
+++ example1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Classes_and_object
+{
+    class Calculator
+    {
+            public int num1;
+            public int num2;
+        public void input()
+        {
+            Console.Write("Enter first number: ");
+            num1= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2= Convert.ToInt32(Console.ReadLine());
+        }
+        public void add()
+        {
+            Console.WriteLine("Addition:" + (num1 + num2));
+        }
+        public void subtract()
+        {
+            Console.WriteLine("Subtraction:" + (num1 - num2));
+        }
+    }
+    class example1
+    {
+        public static void Main(string[] args)
+        {
+            Calculator basic = new Calculator();
+            basic.input();
+            basic.add();
+            basic.subtract();
+            Console.ReadLine();
+        }
+    }
+}
