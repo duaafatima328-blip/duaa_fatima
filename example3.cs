83c768754c52868c7c8c14f3d62ff750e8d88fe8
duaafatima328-blip <duaafatima328@gmail.com>
2025-12-29 16:14:21 +0500

    Add files via upload

---
+++ example3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace example_3
+{
+    class areacalculator
+    {
+        public int length;
+        public int width;
+
+        public void input()
+        {
+            Console.WriteLine("Enter Lenth:");
+            length= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Width:");
+            width= Convert.ToInt32(Console.ReadLine());
+        }
+        public void area()
+        {
+            int area = length * width;
+            Console.WriteLine("Area of Rectangle is: " + area);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            areacalculator obj = new areacalculator();
+            obj.input();
+            obj.area();
+            Console.ReadLine();
+        }
+    }
+    class example3
+    {
+        public static void main(string[]args)
+        {
+            areacalculator rectanglearea= new areacalculator();
+            rectanglearea.input();
+            rectanglearea.area();
+        }
+    }
+}
